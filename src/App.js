7f058650008a29ebcce351f25ab554c87dc33584
Kenyon Blue <bluekenyfleo@gmail.com>
2019-01-06 14:01:04 -0500

    was able to get the header working again
    simulator was not working because of syntax errors within the login
    page. changed the header to s. goals

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text  } from 'react-native';
+// import firebase from 'firebase';
+import { Header } from './components/common';
+// import LoginForm from './LoginForm';
+
 
 class App extends Component {
-    state = {  }
+
+    // componentWillMount() {
+    //     firebase.inializeApp(
+    //         {
+    //             apiKey: "AIzaSyAyjEPxvbGenOxm7QJR4oZN_JR46uistXI",
+    //             authDomain: "authentication-eb5df.firebaseapp.com",
+    //             databaseURL: "https://authentication-eb5df.firebaseio.com",
+    //             projectId: "authentication-eb5df",
+    //             storageBucket: "authentication-eb5df.appspot.com",
+    //             messagingSenderId: "721013450581"
+    //           });
+    // }
+
+    
     render() {
         return (
             <View>
-                <Text>kenyon's app</Text>
+                <Header headerText="Squaaad Goals" /> 
+                {/* <LoginForm /> */}
+                <Text>
+                    hello kenyon
+                </Text>
             </View>
+        
         );
     }
-}
+};
 
 export default App;
---
+++ src/components/LoginForm.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import {Button, Card, CardSection} from './common/ndex';
+
+
+class LoginForm extends Component {
+    
+    render() {
+        return (
+          <Card>
+              <CardSection/>
+              <CardSection/>
+              <CardSection>
+                  <Button>
+                      Login
+                  </Button>
+              </CardSection>
+          </Card> 
+        );
+    }
+}
+
+export default LoginForm;
